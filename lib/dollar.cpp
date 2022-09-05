3fac0a6c7e58b7a5280cddcd826cb843af8e4526
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-05 21:11:59 +0900

    Generalize `equals` to pass the test

--- lib/dollar.cpp
+++ lib/dollar.cpp
@@ -4,4 +4,4 @@ Dollar::Dollar(int amount) { this->amount = amount; };
 Dollar Dollar::times(int multiplier) {
   return Dollar(this->amount * multiplier);
 };
-bool Dollar::equals(Dollar other) { return true; };
+bool Dollar::equals(Dollar other) { return this->amount == other.amount; };
