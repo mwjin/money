61dd60dc94e3c8a2072ff32841813193c8942afc
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-05 20:20:00 +0900

    Use `amount` argument to remove duplication

--- lib/dollar.cpp
+++ lib/dollar.cpp
@@ -1,4 +1,4 @@
 #include "dollar.h"
 
-Dollar::Dollar(int amount){};
-void Dollar::times(int multiplier) { this->amount = 5 * 2; };
+Dollar::Dollar(int amount) { this->amount = amount; };
+void Dollar::times(int multiplier) { this->amount = this->amount * 2; };
