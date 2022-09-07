53968e86dd835be2ec2f1fb13fc2eb2ca50a486c
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-07 10:56:04 +0900

    Rename DollarTest to MoneyTest

--- tests/money_test.cpp
+++ tests/money_test.cpp
@@ -2,13 +2,13 @@
 
 #include <gtest/gtest.h>
 
-TEST(DollarTest, TestMultiplication) {
+TEST(MoneyTest, TestMultiplication) {
   Dollar five(5);
   EXPECT_TRUE(Dollar(10) == five.times(2));
   EXPECT_TRUE(Dollar(15) == five.times(3));
 }
 
-TEST(DollarTest, TestEquality) {
+TEST(MoneyTest, TestEquality) {
   EXPECT_TRUE(Dollar(5) == Dollar(5));
   EXPECT_FALSE(Dollar(5) == Dollar(6));
 }
