c6775972d365947bbeaab4c57294324ccc57bb3c
Minwoo Jeong <jeongmwj@gmail.com>
2022-09-07 10:57:12 +0900

    Add `TestFrancMultiplication`
    Build will be failed.

--- tests/money_test.cpp
+++ tests/money_test.cpp
@@ -12,3 +12,9 @@ TEST(MoneyTest, TestEquality) {
   EXPECT_TRUE(Dollar(5) == Dollar(5));
   EXPECT_FALSE(Dollar(5) == Dollar(6));
 }
+
+TEST(MoneyTest, TestFrancMultiplication) {
+  Franc five(5);
+  EXPECT_TRUE(Franc(10) == five.times(2));
+  EXPECT_TRUE(Franc(15) == five.times(3));
+}
