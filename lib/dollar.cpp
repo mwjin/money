5524c620e04bf61c15eea9ae4e107a2431ee3cd7
Minwoo Jeong <jeongmwj@gmail.com>
2022-10-03 09:14:21 +0900

    Use the base class constructor in `times` methods
    Build will be failed because the `Money` is an abstract class

--- lib/dollar.cpp
+++ lib/dollar.cpp
@@ -2,5 +2,5 @@
 
 Dollar::Dollar(int amount) : Money{amount, "USD"} {};
 std::unique_ptr<Money> Dollar::times(int multiplier) {
-  return std::make_unique<Dollar>(this->amount * multiplier);
+  return std::make_unique<Money>(this->amount * multiplier, currency_);
 };
--- lib/franc.cpp
+++ lib/franc.cpp
@@ -2,5 +2,5 @@
 
 Franc::Franc(int amount) : Money{amount, "CHF"} {}
 std::unique_ptr<Money> Franc::times(int multiplier) {
-  return std::make_unique<Franc>(this->amount * multiplier);
+  return std::make_unique<Money>(this->amount * multiplier, currency_);
 };
